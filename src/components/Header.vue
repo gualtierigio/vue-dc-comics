92c54d05285e469599b80fe69d17be22dfa35e82
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-09-24 17:00:25 +0200

    lavoro sul nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
        <div class="page-wrap d-flex">
             <img src="../assets/img/dc-logo.png" alt="">
             <ul>
-                <li></li>
+                <li v-for="(link, index) in links" :key="index"><a href="{{}}"></a></li>
             </ul>
        </div>
    </header>
@@ -11,11 +11,72 @@
 
 <script>
 export default {
+    name: 'Header',
+    data: function () {
+        return {
+            links: [
+                {
+                    text: "Characters",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Comics",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "Movies",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Tv",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Games",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Collectibles",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Videos",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Fans",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "News",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "Shop",
+                    url: "#",
+                    current: false,
+                }
+            ]
+        }
+    }
 
 }
 </script>
 
 <style scoped lang="scss">
+
+header img{
+    height: 120px;
+}
 
 
 </style>
