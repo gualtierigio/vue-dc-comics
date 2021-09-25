7d69cd877c2c734a021b1a929a5ece27cf585042
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-09-25 14:27:21 +0200

    inizio a impostare i dc-comics

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,65 @@
 <template>
    <main>
-
+      <div class="page-wrap">
+         <div class="content-wrap d-flex">
+            <h2>--&gt; Content goes here &lt;-- </h2>
+            <img src="" alt="">
+         </div>
+      </div>
    </main>
 </template>
 
 <script>
 export default {
+   name: 'Main',
+   data: function () {
+      return {
+         buyComics: [
+            {
+               text: "digital comics",
+               img: "../assets/img/buy-comics-digital-comics.png",
+            },
+            {
+               text: "dc merchandise",
+               img: "../assets/img/buy-comics-merchandise.png",
+            },
+            {
+               text: "subscription",
+               img: "../assets/img/buy-comics-subscription.png",
+            },
+            {
+               text: "comic shop locator",
+               img: "../assets/img/buy-comics-shop-locator.png",
+            },
+            {
+               text: "digital comics",
+               img: "",
+            },
+         ]
+      }
+   }
 
 }
 </script>
 
 <style scoped lang="scss">
+@import "../style/variables.scss";
+
+main{
+   height: 120px;
+   background-color: black;
+   color: $white-color;
+}
+
+div.page-wrap{
+   height: 100%;
+}
+
+div.content-wrap{
+   height: 100%;
+   align-items: center;
+}
+
+
 
 </style>
